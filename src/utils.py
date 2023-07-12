bcaab406baf986546ef05f6e2531f8dd4d4cbb2b
aygalic <aygalic@hotmail.fr>
2023-07-12 16:47:06 +0200

    refacto functional

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,6 @@ from tensorflow.keras.models import Model
 from tensorflow import keras
 
 
-
 # datasets
 
 # default path of the folder containing the salmon files
@@ -40,12 +39,13 @@ def load_patient_data(filename):
 
 
 # building the actual dataset
-def generate_dataset(path = absolute_path, feature_selection_threshold = None, batch_size = 64):
+def generate_dataset(path = absolute_path, feature_selection_threshold = None, batch_size = 64, subsample = None):
     # getting entries ready
     # each couple of entries correspond to one patient, we are only interested in the "transcript" files
     entries = os.listdir(absolute_path)
     entries_transcripts = [e for e in entries if "transcripts" in e ]
-
+    if(subsample is not None):
+        entries_transcripts = entries_transcripts[1:40] # for testing purpose 
     # load the dataset into a list using the first pipeline
     data = [load_patient_data(e) for e in entries_transcripts]
 
@@ -58,6 +58,7 @@ def generate_dataset(path = absolute_path, feature_selection_threshold = None, b
         data_array = np.array(train_ds)
         MAD = scipy.stats.median_abs_deviation(data_array)
         gene_selected = [True if val > feature_selection_threshold else False for val in MAD]
+        print("number of genes selected : ",sum(gene_selected))
         train_ds = data_array[:,gene_selected]
 
     x_train = tf.data.Dataset.from_tensor_slices(train_ds)
@@ -69,9 +70,8 @@ def generate_dataset(path = absolute_path, feature_selection_threshold = None, b
 
 # models
 
-
 class Sampling(layers.Layer):
-    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+    #Uses (z_mean, z_log_var) to sample z, the vector encoding a digit.
 
     def call(self, inputs):
         z_mean, z_log_var = inputs
@@ -82,7 +82,7 @@ class Sampling(layers.Layer):
 
 
 
-class encoder():
+class Encoder():
     def __init__(self, input_shape, latent_dim = 64, **kwargs):
         self.input_shape = input_shape
         self.encoder_inputs = keras.Input(shape=input_shape)
@@ -97,9 +97,9 @@ class encoder():
         self.z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
         self.z = Sampling()([self.z_mean, self.z_log_var])
         self.encoder = keras.Model(self.encoder_inputs, [self.z_mean, self.z_log_var, self.z], name="encoder")
-        self.encoder.summary()
+        #self.encoder.summary()
 
-class decoder():
+class Decoder():
     def __init__(self, output_shape, latent_dim = 64, **kwargs):
         self.latent_inputs = keras.Input(shape=(latent_dim,))
         x = layers.Dense(64, activation="ELU")(self.latent_inputs)
@@ -110,6 +110,10 @@ class decoder():
         self.decoder_outputs = layers.Dense(output_shape, activation="ELU")(x)
         self.decoder = keras.Model(self.latent_inputs, self.decoder_outputs, name="decoder")
         #self.decoder.summary()
+
+
+
+
 
 class VAE(keras.Model):
     def __init__(self, encoder, decoder, **kwargs):
@@ -152,3 +156,8 @@ class VAE(keras.Model):
             "kl_loss": self.kl_loss_tracker.result(),
         }
 
+def generate_model(shape, latent_dim = 64):
+    encoder = Encoder(shape, latent_dim).encoder
+    decoder = Decoder(shape, latent_dim).decoder
+    vae = VAE(encoder, decoder)
+    return vae
