8fc185455fa7cc0a36926ef82efd8e7138cd0773
aygalic <aygalic@hotmail.fr>
2023-10-17 13:26:31 +0200

    adding self attention mechanism

--- src/models/torch_CVAE_1.py
+++ src/models/torch_CVAE_1.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math 
 
 class AttentionModule(nn.Module):
     def __init__(self, in_features):
@@ -16,14 +17,40 @@ class AttentionModule(nn.Module):
         attention_weights = self.attention(x)
         return attention_weights * x
     
+class SelfAttention(nn.Module):
+    def __init__(self, sequence_length, attention_size):
+        super(SelfAttention, self).__init__()
+        self.sequence_length = sequence_length
+        self.attention_size = attention_size
+        
+        self.query_layer = nn.Linear(sequence_length, attention_size, bias=False)
+        self.key_layer = nn.Linear(sequence_length, attention_size, bias=False)
+        self.value_layer = nn.Linear(sequence_length, sequence_length, bias=False)
+
+    def forward(self, x):
+        # x shape: (batch_size, 1, sequence_length)
+        query = self.query_layer(x)
+        key = self.key_layer(x)
+        value = self.value_layer(x)
+        
+        # Calculate attention scores
+        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(self.attention_size)
+        attention = F.softmax(scores, dim=-1)
+        
+        # Multiply scores with value layer
+        attended_values = torch.matmul(attention, value)
+        return attended_values
+    
 class Autoencoder(nn.Module):
-    def __init__(self, shape, latent_dim=64, dropout = 0.1, slope = 0.05, is_variational=False, use_convolution = False):
+    def __init__(self, shape, latent_dim=64, dropout = 0.1, slope = 0.05, is_variational=False, use_convolution = False, attention_size = 64):
         super(Autoencoder, self).__init__()
+        self.input_shape = shape
         self.latent_dim = latent_dim
         self.is_variational = is_variational
         self.use_convolution = use_convolution
         self.use_attention = False
-
+        self.use_self_attention = False
+        self.attention_size = attention_size
         if use_convolution:
             # Define the convolutional layers for the encoder and decoder
             self.encoder = nn.Sequential(
@@ -116,15 +143,18 @@ class Autoencoder(nn.Module):
         self.use_attention = True
         # Attention for the convolutional path
 
-        self.conv_attention = AttentionModule(in_features = self.latent_dim)  
+        self.attention_module = AttentionModule(in_features = self.latent_dim)  
         
+    def add_self_attention(self):
+        self.use_self_attention = True
+        self.attention_module = SelfAttention(self.input_shape, self.attention_size)
+
 
     def encode(self, x):
         x = self.encoder(x)
-
-        # Apply attention here if using convolution
         if self.use_attention:
-            x = self.conv_attention(x)
+            x = self.attention_module(x)
+        
 
         if self.is_variational:
             mean, logvar = self.mu_layer(x), self.logvar_layer(x)
@@ -141,7 +171,11 @@ class Autoencoder(nn.Module):
         return mu + eps*std
         
     def forward(self, x):
-        
+        # Apply attention here if using convolution
+        if self.use_self_attention:
+            x = self.attention_module(x)
+
+
         if self.is_variational:
             mu, log_var = self.encode(x)
             z = self.reparameterize(mu, log_var)
