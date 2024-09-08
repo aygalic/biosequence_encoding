0e3d9aa4bbcfa0ce142f9346d039bb320eec6cf3
aygalic <aygalic@hotmail.fr>
2024-09-08 16:40:52 +0200

    fix: output path

--- rna_code/__init__.py
+++ rna_code/__init__.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 HOME_PATH = Path(__file__).resolve().parent
-OUTPUT_PATH = HOME_PATH / "outputs"
+OUTPUT_PATH = HOME_PATH / '..' / "outputs"
 
 DEVICE = torch.device("mps")
 LOGFILE = OUTPUT_PATH / "log.txt"
