55a3533f478d0e271c5dc2817097ec0498d0fd27
aygalic <aygalic@hotmail.fr>
2024-09-11 10:57:18 +0200

    chore: add static path for outputs

--- rna_code/__init__.py
+++ rna_code/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 HOME_PATH = Path(__file__).resolve().parent
 OUTPUT_PATH = HOME_PATH / '..' / "outputs"
+STATIC_OUTPUT_PATH = HOME_PATH / '..' / "static"
 
 DEVICE = torch.device("mps")
 LOGFILE = OUTPUT_PATH / "log.txt"
