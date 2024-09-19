76336535560093969c4a0a8328d824470527860c
aygalic <aygalic@hotmail.fr>
2024-09-19 13:01:04 +0200

    fix: mlp ae now properly reconstruct data

--- rna_code/models/mlp_ae.py
+++ rna_code/models/mlp_ae.py
@@ -6,10 +6,8 @@ class MLPAutoencoder(Autoencoder):
     def __init__(
             self,
             **kwargs):
-        
         super().__init__(**kwargs)
-        
-        self.layer_sizes = [self.input_shape] + [1024 // (2 ** i) for i in range(self.num_layers - 1)] + [self.latent_dim]
+        self.layer_sizes = [self.input_shape] + [2048 // (2 ** i) for i in range(self.num_layers - 1)] + [self.latent_dim]
 
     def build_encoder(self):
         encoder_layers = []
@@ -31,5 +29,12 @@ class MLPAutoencoder(Autoencoder):
                 nn.LeakyReLU(self.slope),
                 nn.Dropout(self.dropout)
             ])
+        decoder_layers.append(nn.Linear(self.layer_sizes[-1], self.layer_sizes[-1]),)
         decoder_layers.append(nn.Sigmoid())
         self.decoder = nn.Sequential(*decoder_layers)
+
+class DebugModule(nn.Module):
+    def forward(self, x):
+        print(x.shape)
+        breakpoint()
+        return x
