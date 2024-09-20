eeda6c6bc5bd2178baa8bf1f4342a374cf6e2159
aygalic <aygalic@hotmail.fr>
2024-09-20 10:46:03 +0200

    doc: docstring base feature selector

--- rna_code/data/feature_selection/base_feature_selector.py
+++ rna_code/data/feature_selection/base_feature_selector.py
@@ -1,56 +1,51 @@
-"""
-Feature Selection Module
-
-This module provides a set of classes for feature selection in datasets, particularly focusing on genomic data.
-It includes various statistical and machine learning methods to identify the most significant features for modeling.
-
-Classes:
-- BaseFeatureSelector: Abstract base class for all feature selectors.
-- StatisticalSelector: Implements statistical methods like MAD and expression-based selection.
-- LaplacianSelector: Implements Laplacian Score-based selection.
-- LassoSelector: Implements LASSO regression-based selection.
-
-Usage:
-    from feature_selection import StatisticalSelector, LaplacianSelector, LassoSelector
-    
-    stat_selector = StatisticalSelector()
-    selected_features_mad = stat_selector.mad_selection(data_array, threshold=0.5)
-    
-    lap_selector = LaplacianSelector()
-    selected_features_ls = lap_selector.ls_selection(data_array, threshold=0.1)
-    
-    lasso_selector = LassoSelector()
-    selected_features_lasso = lasso_selector.lasso_selection(data_array, labels)
-
-Note:
-The module is designed with genomic datasets in mind, but may be applicable to other types of numerical datasets.
-"""
+"""Abstract class for feature selection"""
 
 from abc import ABC, abstractmethod
+import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
 
+
 class BaseFeatureSelector(ABC):
-    def __init__(self, threshold : float | None = None):
+    """Base Abstract class for FeatureSelectors
+
+    Parameters
+    ----------
+    threshold : float | None, optional
+        Selection threshold for given task, by default None
+    """
+    def __init__(self, threshold: float | None = None):
         self.scaler = StandardScaler()
-        self.scores : list[float] = []
-        self.threshold : float | None = threshold
-        self._plot_title : str = ""
-        self._plot_x_label : str = 'Scores Value'
-        self._plot_range_values : list[float] = [0, 0.01]
+        self.scores: list[float] = []
+        self.threshold: float | None = threshold
+        self._plot_title: str = ""
+        self._plot_x_label: str = "Scores Value"
+        self._plot_range_values: list[float] = [0, 0.01]
 
     @abstractmethod
-    def select_features(self, data_array, **kwargs):
-        pass
+    def select_features(self, data_array : np.ndarray, **kwargs) -> np.ndarray:
+        """Select features from data array according to self.threshold.
+
+        Parameters
+        ----------
+        data_array : np.ndarray
+            Data to select features from.
+
+        Returns
+        -------
+        np.ndarray
+            Filtered data.
+        """
 
     def _plot_distribution(self):
+        """Plot data distribution with given threshold."""
         plt.figure(figsize=(10, 5))
-        plt.hist(self.scores, bins=100, color='blue', range=self._plot_range_values)
-        plt.axvline(self.threshold, color='red', linestyle='--', label='Threshold')
+        plt.hist(self.scores, bins=100, color="blue", range=self._plot_range_values)
+        plt.axvline(self.threshold, color="red", linestyle="--", label="Threshold")
         plt.title(self._plot_title)
         plt.xlabel(self._plot_x_label)
-        plt.ylabel('Frequency')
+        plt.ylabel("Frequency")
         plt.legend()
         plt.grid(True)
         plt.show()
