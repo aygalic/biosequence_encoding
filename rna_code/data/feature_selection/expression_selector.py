e8af5810eb3e2b0ff28205cc43c876f90547794e
aygalic <aygalic@hotmail.fr>
2024-09-22 18:55:56 +0200

    docstring

--- rna_code/data/feature_selection/expression_selector.py
+++ rna_code/data/feature_selection/expression_selector.py
@@ -1,15 +1,26 @@
+"""Expression level based selection module"""
+
 import logging
-from .base_feature_selector import BaseFeatureSelector
+
 import numpy as np
 
-class ExpressionSelector(BaseFeatureSelector):
+from .base_feature_selector import BaseFeatureSelector
 
-    def __init__(self, threshold : float | None = None):
+
+class ExpressionSelector(BaseFeatureSelector):
+    """Feature selection based on expression threshold
+
+    Parameters
+    ----------
+    threshold : float | None, optional
+        Expression threshold, by default None
+    """
+    def __init__(self, threshold: float | None = None):
         super().__init__(threshold)
         self._plot_title = "Distribution of Non Zero values per genes"
-        self._plot_range_values : list[float] = [0, 1]
+        self._plot_range_values: list[float] = [0, 1]
 
-    def select_features(self, data_array):
+    def select_features(self, data_array)  -> np.ndarray:
         """
         Selects features based on gene expression levels.
 
@@ -24,6 +35,6 @@ class ExpressionSelector(BaseFeatureSelector):
         selection = [val > (1 - self.threshold) for val in self.scores]
         logging.info(
             "removing %i genes under the expression threshold from the dataset",
-            len(selection)-sum(selection))
+            len(selection) - sum(selection),
+        )
         return selection
-
