bd66c9fb9f0781fd225550e7fcdb925b8332efdf
aygalic <aygalic@hotmail.fr>
2024-09-23 17:32:15 +0200

    docstring laplacian selector

--- rna_code/data/feature_selection/laplacian_selector.py
+++ rna_code/data/feature_selection/laplacian_selector.py
@@ -1,4 +1,7 @@
+"""Module for Laplacian Score based feature selection"""
+
 import logging
+
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
@@ -6,7 +9,17 @@ from .base_feature_selector import BaseFeatureSelector
 
 
 class LaplacianSelector(BaseFeatureSelector):
-    def __init__(self, threshold : float | None = None, k : int = 5):
+    """Feature selection based on Laplacian score.
+
+    Parameters
+    ----------
+    threshold : float | None, optional
+        Laplacian score threshold, by default None
+    k : int, optional
+        Number of neighbors for the kNN algorithm, by default 5
+    """
+
+    def __init__(self, threshold: float | None = None, k: int = 5):
         super().__init__(threshold)
         self.k = k
         self._plot_title = "Distribution of Laplacian Score (LS)"
@@ -22,20 +35,20 @@ class LaplacianSelector(BaseFeatureSelector):
         Returns:
             numpy.ndarray: Array of Laplacian scores for each feature.
         """
-        dists = squareform(pdist(X, metric='euclidean'))
-        dists_knn = np.sort(dists)[:, 1:self.k+1]
+        dists = squareform(pdist(X, metric="euclidean"))
+        dists_knn = np.sort(dists)[:, 1 : self.k + 1]
         sigma = np.mean(dists_knn)
-        W = np.exp(-dists ** 2 / (2 * sigma ** 2))
+        W = np.exp(-(dists**2) / (2 * sigma**2))
         D = np.diag(np.sum(W, axis=1))
         L = D - W
         D_inverse_sqrt = np.diag(1 / np.sqrt(np.diag(D)))
         S = D_inverse_sqrt @ L @ D_inverse_sqrt
-        
+
         fraternities = np.zeros(X.shape[1])
         for i in range(X.shape[1]):
             f = X[:, i] - np.mean(X[:, i])
             fraternities[i] = f.T @ S @ f / (f.T @ D @ f)
-        
+
         return fraternities
 
     def select_features(self, data_array):
@@ -55,5 +68,6 @@ class LaplacianSelector(BaseFeatureSelector):
         selection = [self.threshold < val < 100 for val in self.scores]
         logging.info(
             "removing %i genes outside the Laplacian score window from the dataset",
-            len(selection)-sum(selection))
+            len(selection) - sum(selection),
+        )
         return selection
