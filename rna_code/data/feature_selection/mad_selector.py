5805009f2e8c5b7d96987800472ed15533a25ac7
aygalic <aygalic@hotmail.fr>
2024-09-24 10:27:26 +0200

    doc: docstring mad selection

--- rna_code/data/feature_selection/mad_selector.py
+++ rna_code/data/feature_selection/mad_selector.py
@@ -1,25 +1,36 @@
+"""Module for Mean absolute deviation based feature selection"""
+
 import logging
+
+import numpy as np
 import scipy
 
 from .base_feature_selector import BaseFeatureSelector
 
 
 class MADSelector(BaseFeatureSelector):
-    def __init__(self, threshold : float | None = None, ceiling : int = 150):
+    """Feature selection based on Mean Absolute Deviation threshold
+
+    Parameters
+    ----------
+    threshold : float
+        Minimum threshold for variables, by default None
+    ceiling : int, optional
+        Maximum value to prevent outliers, by default 150
+    """
+
+    def __init__(self, threshold: float, ceiling: int = 150):
         super().__init__(threshold)
         self.ceiling = ceiling
         self._plot_title = "Distribution of Median Absolute Deviation (MAD)"
-        self._plot_range_values : list[float] = [0, ceiling + 20]
+        self._plot_range_values: list[float] = [0, ceiling + 20]
 
-
-
-    def select_features(self, data_array ):
+    def select_features(self, data_array: np.ndarray) -> list:
         """
         Selects features based on Median Absolute Deviation (MAD).
 
         Parameters:
             data_array (numpy.ndarray): The dataset to process.
-            threshold (float): The lower threshold for feature selection.
 
         Returns:
             list: A list of boolean values indicating selected features.
@@ -28,5 +39,6 @@ class MADSelector(BaseFeatureSelector):
         selection = [self.threshold < val < self.ceiling for val in self.scores]
         logging.info(
             "removing %i genes outside the MAD window from the dataset",
-            len(selection)-sum(selection))
+            len(selection) - sum(selection),
+        )
         return selection
