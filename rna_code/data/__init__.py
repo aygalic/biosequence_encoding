feb56356b7176a0f6cf44c40fb8ca5c2f905c615
aygalic <aygalic@hotmail.fr>
2024-11-07 18:04:39 +0100

    chore: CPTAC-3 data path

--- rna_code/data/__init__.py
+++ rna_code/data/__init__.py
@@ -4,3 +4,8 @@ DATA_PATH = HOME_PATH / ".." / "data"
 BRCA_DATA_PATH = DATA_PATH / "BRCA"
 BRCA_METADATA_FILE = BRCA_DATA_PATH / "metadata.cart.2023-09-22.json"
 BRCA_SUBTYPES_FILE = BRCA_DATA_PATH / "patient_subtype.tsv"
+
+CPTAC_3_DATA_PATH = DATA_PATH / "CPTAC-3"
+BRCA_METADATA_FILE = CPTAC_3_DATA_PATH / "metadata.repository.2024-11-07.json"
+
+
