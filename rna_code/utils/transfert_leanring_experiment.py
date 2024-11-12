8da7fff8b119ef4e74a0bdb43b01d608efd50210
aygalic <aygalic@hotmail.fr>
2024-11-12 17:32:32 +0100

    fix: transfert learning experiment

--- rna_code/utils/transfert_leanring_experiment.py
+++ rna_code/utils/transfert_leanring_experiment.py
@@ -1,20 +1,18 @@
 """Experiment module, in charge of handling data loading, model training and monitoring
-according to provided parameters.
-"""
-from pathlib import Path
+according to provided parameters."""
 
-import pandas as pd
-import pytorch_lightning as pl
 import numpy as np
+import pytorch_lightning as pl
 
 from rna_code.data.data_module.brca_data_module import BRCADataModule
+from rna_code.data.data_module.cptac_3_data_module import CPTAC3DataModule
 from rna_code.utils.monitor_callback import MonitorCallback
 
-from .. import DEVICE, LOGFILE
-
+from .. import DEVICE
 from ..models.model_builder import ModelBuilder
 from . import visualization
 from .experiment import Experiment
+
 
 class TransfertLearningExperiment(Experiment):
     """Experiment class handling data, model training and monitoring.
@@ -26,19 +24,16 @@ class TransfertLearningExperiment(Experiment):
     model_param : dict
         Model related parameters.
     """
-    def __init__(
-            self,
-            pre_train_data_param : dict,
-            data_param : dict,
-            model_param : dict
-            ) -> None:
 
+    def __init__(
+        self, pre_train_data_param: dict, data_param: dict, model_param: dict
+    ) -> None:
         self.pre_train_data_param = pre_train_data_param
         self.data_param = data_param
         self.model_param = model_param
 
         self.n_epoch = self.model_param.pop("n_epoch", 10)
-        self.pretrain_data_module = BRCADataModule(pre_train_data_param)
+        self.pretrain_data_module = CPTAC3DataModule(pre_train_data_param)
         self.data_module = BRCADataModule(data_param)
         self.pretrain_data_module.setup(stage=None)
         self.data_module.setup(stage=None)
@@ -47,8 +42,7 @@ class TransfertLearningExperiment(Experiment):
         self.input_shape = self.data_module.feature_num
 
         self.model_builder = ModelBuilder(self.input_shape, self.model_param)
-        self.model : pl.LightningModule
-
+        self.model: pl.LightningModule
 
     def run(self) -> None:
         """Run experiment.
@@ -59,29 +53,27 @@ class TransfertLearningExperiment(Experiment):
 
         monitoring_interval = np.unique([int(x) for x in np.logspace(1, 3, num=50)])
         monitor_callback = MonitorCallback(
-             dataloader=self.data_module.full_data_loader(),
-             labels=self.data_module.full_meta_data["subtypes"],
-             n_clusters=5,
-             compute_on='batch',
-             evaluation_intervals = monitoring_interval,
-             verbose=0
+            dataloader=self.data_module.full_data_loader(),
+            labels=self.data_module.full_meta_data["subtypes"],
+            n_clusters=5,
+            compute_on="batch",
+            evaluation_intervals=monitoring_interval,
+            verbose=0,
         )
         trainer = pl.Trainer(max_epochs=self.n_epoch, callbacks=[monitor_callback])
         trainer.fit(self.pretrain_data_module, self.data_module)
         trainer.fit(self.model, self.data_module)
         visualization.post_training_viz(
-            data = self.data_module.data_array,
-            dataloader = self.data_module.full_data_loader(),
-            model =  self.model,
-            DEVICE = DEVICE,
-            loss_hist = monitor_callback.loss_values,
-            labels = self.data_module.full_meta_data["subtypes"]
-            )
+            data=self.data_module.data_array,
+            dataloader=self.data_module.full_data_loader(),
+            model=self.model,
+            DEVICE=DEVICE,
+            loss_hist=monitor_callback.loss_values,
+            labels=self.data_module.full_meta_data["subtypes"],
+        )
         print(monitor_callback.metrics[-1])
         record = {**self.data_param, **self.model_param, **monitor_callback.metrics[-1]}
         Experiment._log_experiment(record)
         visualization.post_training_animation(
-            monitor = monitor_callback,
-            metadata = self.data_module.full_meta_data)
-
-
+            monitor=monitor_callback, metadata=self.data_module.full_meta_data
+        )
