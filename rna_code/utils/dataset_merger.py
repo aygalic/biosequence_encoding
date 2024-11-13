c64913a616aa24e663502a46e9e6e51295444a4f
aygalic <aygalic@hotmail.fr>
2024-11-13 17:36:57 +0100

    feat: prototype for dataset merger

---
+++ rna_code/utils/dataset_merger.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class DatasetMerger():
+
+    @staticmethod
+    def intersect(dataset1 : pd.DataFrame, dataset2: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+        pass
+
+    @staticmethod
+    def union(dataset1 : pd.DataFrame, dataset2: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+        pass
