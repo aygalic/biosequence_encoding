5250591fa5c26bd3158c4e53c2499dc27e8391fa
aygalic <aygalic@hotmail.fr>
2024-11-14 17:01:11 +0100

    chore: implement DatasetMerger.intersect

--- rna_code/utils/dataset_merger.py
+++ rna_code/utils/dataset_merger.py
@@ -4,7 +4,8 @@ class DatasetMerger():
 
     @staticmethod
     def intersect(dataset1 : pd.DataFrame, dataset2: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
-        pass
+        feature_intersection = set(dataset1.columns) & set(dataset2.columns)
+        return pd.concat(dataset1[feature_intersection], dataset2[feature_intersection], axis = 1)
 
     @staticmethod
     def union(dataset1 : pd.DataFrame, dataset2: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
