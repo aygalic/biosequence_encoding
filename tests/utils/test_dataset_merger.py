b37e4cda1770c80b2117a282081b61a6d9b7c382
aygalic <aygalic@hotmail.fr>
2024-11-14 18:23:22 +0100

    ci: implemented tests for dataset merger

---
+++ tests/utils/test_dataset_merger.py
@@ -0,0 +1,22 @@
+import unittest
+import pandas as pd
+from rna_code.utils.dataset_merger import DatasetMerger
+
+class TestDatasetMerger(unittest.TestCase):
+    def test_intersect_dataset(self):
+
+        dataset_1 = pd.DataFrame(data = {
+            "a" : [1,2],
+            "b" : [3,4]
+        })
+        dataset_2 = pd.DataFrame(data = {
+            "a" : [1,2],
+            "c" : [3,4]
+        })
+        merged_df = DatasetMerger.intersect(dataset_1, dataset_2)
+        assert merged_df == pd.DataFrame(data = {"a" : [1,2,1,2]})
+
+
+
+if __name__ == "__main__":
+    unittest.main()
