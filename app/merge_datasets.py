d5a448da0b4a775edb9235994f92c73112e9cc40
aygalic <aygalic@hotmail.fr>
2024-11-15 11:11:38 +0100

    feat: implement merger script

--- app/merge_datasets.py
+++ app/merge_datasets.py
@@ -1,6 +1,27 @@
+"""Merge datasets."""
+import logging
+import pandas as pd 
+
+
+from rna_code import CACHE_PATH
+from rna_code.utils.dataset_merger import DatasetMerger
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+logger = logging.getLogger(__name__)
+
+logger.info("Merging data..")
 
 def main():
-    pass
+    data_path_BRCA = CACHE_PATH / "data"/ 'BRCA_data.csv'
+    data_path_CPTAC_3 = CACHE_PATH / "data"/ 'CPTAC_3_data.csv'
+    data_BRCA = pd.read_csv(data_path_BRCA, index_col=0).values
+    data_CPTAC_3 = pd.read_csv(data_path_CPTAC_3, index_col=0).values
+    merged_data = DatasetMerger.intersect(data_BRCA, data_CPTAC_3)
+    
+
 
 if __name__ == "__main__":
     main()
